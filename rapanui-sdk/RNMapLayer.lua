79ae678b2c6356589d649fee9158f6792cfefaf5
Burning Daylight <dmitry.v.potapov@gmail.com>
2013-02-01 15:49:55 +0400

    rapanui-sdk/RNMapLayer.lua: support drawing of tilesets with firstgid != 1

--- rapanui-sdk/RNMapLayer.lua
+++ rapanui-sdk/RNMapLayer.lua
@@ -159,6 +159,10 @@ function RNMapLayer:initLayer(x, y, tileset, drawmode)
     self.grid:setRepeat(false)
 
     self.grid:setSize(self:getCols(), self:getRows(), tileset:getTileWidth(), tileset:getTileHeight(), 0, 0, tileset:getTileWidth(), tileset:getTileHeight())
+    local tile_idx_delta = 0
+    if tileset.firstgid then
+      tile_idx_delta = tileset.firstgid - 1
+    end
 
     for row = 0, self:getRows() - 1 do
         local cols = {}
@@ -166,7 +170,7 @@ function RNMapLayer:initLayer(x, y, tileset, drawmode)
             local tileIdx = self:getTilesAt(row, col)
             local tileIsPhysical = tileset:getPropertyValueForTile(tileIdx, "isPhysical")
             if tileIsPhysical == nil then
-                table.insert(cols, tileIdx)
+                table.insert(cols, tileIdx - tile_idx_delta)
             else
                 table.insert(cols, 0)
             end
